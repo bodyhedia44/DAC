ab8e9a04432c4da93e829f26a51bb6624d9088f7
Abdelrahman Hedia <66520407+bodyhedia44@users.noreply.github.com>
2023-08-04 12:13:57 +0200

    Update POSAppController.php

--- app/Http/Controllers/POSAppController.php
+++ app/Http/Controllers/POSAppController.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Setting;
 use Illuminate\Http\Request;
 
 class POSAppController extends Controller
 {
     public function get_items(){
+        $s=Setting::first();
+
         $categoriesWithProducts = Category::with('products')->get();
         return response()->json([
             'status' => true,
+            'tax' =>$s->tax,
             'message' => 'تم جلب البيانات',
             'data'=>$categoriesWithProducts,
         'code'=>200
